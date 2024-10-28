47889acf35524ccb7efaa0e1ad4cf8f0b9298ec4
JayeshPatil18 <jp7470484@gmail.com>
2024-10-29 00:55:35 +0530

    may work

--- src/socket.tsx
+++ src/socket.tsx
@@ -2,6 +2,6 @@
 import { io } from 'socket.io-client';
 
 // Create and export the socket connection
-const socket = io('http://localhost:3000');
+const socket = io('https://websocket-randomchat.onrender.com');
 
 export default socket;
